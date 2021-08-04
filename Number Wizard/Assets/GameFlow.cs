d6ce617cda83350aa8282d2c86fd4a8a509e6217
cagri-eroglu <71547123+cagri-eroglu@users.noreply.github.com>
2021-08-04 09:39:20 +0300

    Number Wizard
    with Unity

---
+++ Number Wizard/Assets/GameFlow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameFlow : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] int guess;
+    [SerializeField] TextMeshProUGUI guesstext;
+
+    private void Start()
+    {
+        guess = Random.Range(min,max+1);
+        guesstext.text = guess.ToString();
+    }
+    private void Update()
+    {
+        
+    }
+    private void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guesstext.text = guess.ToString();
+    }
+    public void OnPressHigher()
+    {
+        max = guess + 1;
+        NextGuess();
+    }
+   
+    public void onPressLower()
+    {
+        min = guess - 1;
+        NextGuess();
+    }
+}
